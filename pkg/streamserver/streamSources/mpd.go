ce2cfe2701b76727132dd7abab432518bd39aefd
Alexandre Pires <c.alexandre.pires@gmail.com>
2025-03-29 18:57:03 +0100

    Refactor MPDStreamSource to improve request handling and base URL remapping

--- pkg/streamserver/streamSources/mpd.go
+++ pkg/streamserver/streamSources/mpd.go
@@ -3,6 +3,7 @@ package streamSources
 import (
 	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"path"
@@ -16,91 +17,110 @@ func (stream *MPDStreamSource) Serve(w http.ResponseWriter, r *http.Request, tim
 
 	vars := mux.Vars(r)
 
-	var uri *url.URL
-	rPath := vars["rPath"]
-	if rPath[0:4] == "mpd-" {
-		// get index of the first slash after mpd/
+	document := vars["path"]
+	if document == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if document == "master.mpd" {
+		// if the cache is empty, we must be serving the master playlist
+		uri, _ := url.Parse(stream.m3u.URI)
+
+		resp, err := executeRequest("GET", uri.String(), stream.transport, stream.headers, timeout)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		ct := resp.Header.Get("Content-Type")
+		_, _, err = contenttype.GetAcceptableMediaTypeFromHeader(ct, supportedMediaTypes)
+		if err != nil {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			return
+		}
+
+		w.Header().Set("Content-Type", ct)
+
+		mpdPlaylist, err := mpd.DecodeFromReader(resp.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		for i := range mpdPlaylist.Period {
+			for j := range mpdPlaylist.Period[i].AdaptationSets {
+				for k := range mpdPlaylist.Period[i].AdaptationSets[j].Representations {
+
+					if len(mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL) == 0 {
+						uri := new(url.URL)
+						uri.Scheme = resp.Request.URL.Scheme
+						uri.Host = resp.Request.URL.Host
+						basePath := path.Dir(resp.Request.URL.Path)
+						uri.Path = path.Join(basePath, uri.Path)
+						remap := base64.URLEncoding.EncodeToString([]byte(uri.String()))
+						mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL = append(mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL, &mpd.BaseURL{Value: fmt.Sprintf("mpd-%s/", remap)})
+						continue
+					} else {
+						for l := range mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL {
+							currentBaseURL := mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL[l].Value
+
+							uri, _ := url.Parse(currentBaseURL)
+
+							if uri.Scheme == "" {
+								uri.Scheme = resp.Request.URL.Scheme
+								uri.Host = resp.Request.URL.Host
+								basePath := path.Dir(resp.Request.URL.Path)
+								uri.Path = path.Join(basePath, uri.Path)
+							}
+
+							remap := base64.URLEncoding.EncodeToString([]byte(uri.String()))
+							mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL[l].Value = fmt.Sprintf("mpd-%s/", remap)
+						}
+					}
+				}
+			}
+		}
+
+		mpdPlaylist.WriteTo(w)
+
+	} else if len(document) > 4 && document[:4] == "mpd-" {
+		// get index of the first slash after mpd-
 		index := 4
-		for i := 4; i < len(rPath); i++ {
-			if rPath[i] == '/' {
+		for i := 4; i < len(document); i++ {
+			if document[i] == '/' {
 				index = i
 				break
 			}
 		}
-		encodedUrl := rPath[4:index]
+		encodedUrl := document[4:index]
 		baseUrl, err := base64.URLEncoding.DecodeString(encodedUrl)
 		if err != nil {
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
-		targetUrl := rPath[index+1:]
-		uri, _ = url.Parse(string(baseUrl) + "/" + targetUrl)
-	} else if vars["path"] == "master.mpd" {
-		// if the cache is empty, we must be serving the master playlist
-		uri, _ = url.Parse(stream.m3u.URI)
+		targetUrl := document[index+1:]
+		uri, _ := url.Parse(string(baseUrl) + "/" + targetUrl)
+
+		resp, err := executeRequest("GET", uri.String(), stream.transport, stream.headers, timeout)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		ct := resp.Header.Get("Content-Type")
+		_, _, err = contenttype.GetAcceptableMediaTypeFromHeader(ct, supportedMediaTypes)
+		if err != nil {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			return
+		}
+
+		w.Header().Set("Content-Type", ct)
+		io.Copy(w, resp.Body)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	mediaURI := uri.String()
-
-	resp, err := executeRequest("GET", mediaURI, stream.transport, stream.headers, timeout)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	ct := resp.Header.Get("Content-Type")
-	_, _, err = contenttype.GetAcceptableMediaTypeFromHeader(ct, supportedMediaTypes)
-	if err != nil {
-		w.WriteHeader(http.StatusUnsupportedMediaType)
-		return
-	}
-
-	w.Header().Set("Content-Type", ct)
-
-	mpdPlaylist, err := mpd.DecodeFromReader(resp.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	for i := range mpdPlaylist.Period {
-		for j := range mpdPlaylist.Period[i].AdaptationSets {
-			for k := range mpdPlaylist.Period[i].AdaptationSets[j].Representations {
-
-				if len(mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL) == 0 {
-					uri := new(url.URL)
-					uri.Scheme = resp.Request.URL.Scheme
-					uri.Host = resp.Request.URL.Host
-					basePath := path.Dir(resp.Request.URL.Path)
-					uri.Path = path.Join(basePath, uri.Path)
-					remap := base64.URLEncoding.EncodeToString([]byte(uri.String()))
-					mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL = append(mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL, &mpd.BaseURL{Value: fmt.Sprintf("mpd-%s/", remap)})
-					continue
-				} else {
-					for l := range mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL {
-						currentBaseURL := mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL[l].Value
-
-						uri, _ := url.Parse(currentBaseURL)
-
-						if uri.Scheme == "" {
-							uri.Scheme = resp.Request.URL.Scheme
-							uri.Host = resp.Request.URL.Host
-							basePath := path.Dir(resp.Request.URL.Path)
-							uri.Path = path.Join(basePath, uri.Path)
-						}
-
-						remap := base64.URLEncoding.EncodeToString([]byte(uri.String()))
-						mpdPlaylist.Period[i].AdaptationSets[j].Representations[k].BaseURL[l].Value = fmt.Sprintf("mpd-%s/", remap)
-					}
-				}
-			}
-		}
-	}
-
-	mpdPlaylist.WriteTo(w)
 }
 
 func (stream *MPDStreamSource) MasterPlaylist() string {
